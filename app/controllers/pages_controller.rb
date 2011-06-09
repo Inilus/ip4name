bae01054eb3cd753d33d39bf1a0218f58723a3ae
Inilus ZIO <inilus@gmail.com>
2011-06-09 14:59:11 +0400

    Desine pages#home for signout users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
+    @domains = [ [ "ip4name.com/anonym/", "type1" ], [ "anonym.ip4name.com/", "type2" ] ]
   end
 
   def about
