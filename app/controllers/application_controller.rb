d3ce425e0dfdcfe24d24bd24117f0271889a8e1e
Inilus ZIO <inilus@gmail.com>
2011-06-09 09:49:37 +0400

    Add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :set_locale
+
   helper_method :current_user
   helper_method :user_signed_in?
 
   private
+
+    def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+    end
+
     def current_user
       @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
     end
